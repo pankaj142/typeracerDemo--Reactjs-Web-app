2e277972298901ed05c40d9c4044df05c198b3d1
Pankaj Agham <pankajagham1@gmail.com>
2018-01-19 19:38:18 +0530

    add fuctionality of matching the entered data

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import request from 'superagent';
 
+
 export default class Home extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			data:"",
+			data:"This is dumy paragraph data.",
 			inputValue:"",
-			count:0
+			count:0,
+			wpm:0,
+			minutes:1,
+			showParagraph:false,
+			showStartButton:false,
+			showInput:false
 		}
 	}
 	
@@ -24,10 +30,37 @@ export default class Home extends React.Component{
 	componentDidMount(){
 		this.getData();
 	}
+	showData(){
+		this.setState({
+			showParagraph:true,
+			showStartButton:true
+		})
+	}
+	countingWords(){
+		setTimeout(()=>{
+			this.setState({
+				showInput:false
+			})
+			console.log("count",this.state.count)
+			
+		},120000)
+
+		setInterval(()=>{
+				console.log("hi")
+				let wpm=this.state.count /this.state.minutes;
+				this.setState({
+					minutes:this.state.minutes,
+					wpm: wpm
+				})
+			},6000)
+
+	}
 	start(){
 		this.setState({
-			data:"This is dumy paragraph data."
+			showInput:true,
+			showStartButton:false
 		})
+		this.countingWords();
 	}
 	handleInputChange(event){
 		this.setState({
@@ -37,12 +70,14 @@ export default class Home extends React.Component{
 	checking(){
 		let words=this.state.data.split(" ")
 		let count=this.state.count;
+		let minutes= this.state.minutes;
 		console.log(words)
 		if(this.state.inputValue === words[count]){
 			console.log("yes")
 			this.setState({
 				inputValue:"",
-				count: +1
+				count: count +1,
+				wpm: count/minutes
 			})
 		}else{
 			console.log("no")
@@ -60,21 +95,47 @@ export default class Home extends React.Component{
 		}
 	}
 	render(){
-		   let text=this.state.data;
-		return(
-				<div className="container">
-					<div className="row">
-						<button onClick={()=>{this.start()}}>Start</button>
-					</div>
-					<div className="row">
-					   {text}
-					</div>
-					<div className="row">
-					   <input 
+		let paragraph;
+ 		if(this.state.showParagraph){
+		   paragraph=this.state.data;
+ 		}else{
+ 			paragraph="";
+ 		}
+ 		let startButton=null;
+
+ 		if(this.state.showStartButton){
+ 				startButton= <div className="form-group">
+		 				 		The race starts in 5 seconds.
+								<button type="button" className="btn"
+								   onClick={()=>{this.start()}}>Start</button>
+							</div>
+ 		}
+ 		let inputfield=null;
+ 		if(this.state.showInput){
+ 			inputfield= <input className="form-control" id="ex1" type="text"
+					            placeholder={"Type the above paragraph here..."}
                                 value={this.state.inputValue}
                                 onChange={(e)=>{this.handleInputChange(e)}}
                                 onKeyPress={(e)=>{this.handleEnter(e)}}
-                        />
+					    />
+
+ 		}
+ 		
+
+		return(
+				<div className="container">
+					<div className="form-group">
+
+						<button type="button" className="btn"
+						   onClick={()=>{this.showData()}}>Show Paragraph</button>
+						   {this.state.wpm}
+					</div>
+					<div className="form-group">
+					   {paragraph}
+					</div>
+					<div className="form-group">
+				    	{startButton}
+					    {inputfield}
 					</div>
 				</div>
 			);
